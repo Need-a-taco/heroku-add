0a988f00cc40a4f947bb55c7965db04a758dc9c5
Joshua Zhang <joshua_zhang@college.harvard.edu>
2025-03-02 10:05:56 -0500

    have add and app

--- add.py
+++ add.py
@@ -1,18 +1,2 @@
-# app.py
-from flask import Flask, request, jsonify
-from add import add
-
-app = Flask(__name__)
-
-@app.route('/add', methods=['POST'])
-def add_route():
-    data = request.get_json()
-    result = add(data['x'], data['y'])
-    return jsonify({'result': result})
-
-@app.route('/')
-def home():
-    return "Addition API is running!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+def add(x, y):
+    return x + y
